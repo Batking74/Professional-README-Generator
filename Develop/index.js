0cab510b29d2088ea49b274217a6dac31472c521
Nazir Knuckles <nazirknuckles@gmail.com>
2023-11-28 15:55:54 -0500

    Completed README Generator

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,99 @@
 // TODO: Include packages needed for this application
+const generater = require('./utils/generateMarkdown.js');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your repository? '
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a repository description: '
+    },
+    {
+        type: 'input',
+        name: 'question1',
+        message: 'What was your motivation?'
+    },
+    {
+        type: 'input',
+        name: 'question2',
+        message: 'Why did you build this project?'
+    },
+    {
+        type: 'input',
+        name: 'question3',
+        message: 'What problem does it solve?'
+    },
+    {
+        type: 'input',
+        name: 'question4',
+        message: 'What did you learn? '
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project? '
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project step by step? '
+    },
+    {
+        type: 'input',
+        name: 'contributers',
+        message: 'Who have you contributed with? '
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the some tests for this project? '
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have? (Use Array keys)',
+        choices: ['MIT', 'Apache 2.0', 'GPLv3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Whats your GitHub username? '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Whats your Email? '
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generater.generateMarkdown(data), (error) => {
+        if(error) throw error;
+    });
+    return "Completed!";
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+        const res = writeToFile('README.md', answers);
+        console.log(res)
+        
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,88 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge;
+  if(license === 'CC0-1.0') {
+    badge = '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)]';
+  }
+  else if(license === 'MIT') {
+    badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+  }
+  else if(license === 'Apache 2.0') {
+    badge = '![Apache 2.0 License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+  }
+  else if(license === 'GPLv3') {
+    badge = '![GPLv3 License](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let link;
+  if(license === 'CC0-1.0') {
+    link = '[CC0-1.0](http://creativecommons.org/publicdomain/zero/1.0/)';
+  }
+  else if(license === 'MIT') {
+    link = '[MIT](https://opensource.org/licenses/MIT)';
+  }
+  else if(license === 'Apache 2.0') {
+    link = '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
+  }
+  else if(license === 'GPLv3') {
+    link = '[GPLv3](https://www.gnu.org/licenses/gpl-3.0)';
+  }
+  return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license != 'None') {
+    return (`
+  ## License
+  ### Notice: ${license}
+  ${renderLicenseBadge(license)}
+  ${renderLicenseLink(license)}
+    `)
+  }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Questions
+  - What was my motivation? ${data.question1}
+  - Why did I build this project? ${data.question2}
+  - What problem does ${data.title} solve? ${data.question3}
+  - What did I learn? ${data.question4}
+
+  GitHub: https://github.com/${data.github}
+
+  Email: ${data.email}
+  
+  ## Installation
+  ${data.installation}
+  
+  
+  ## Usage
+  ${data.usage}
+  
+  ## Credits
+  ${data.contributers}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Tests
+  ${data.tests}
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown};
